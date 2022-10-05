423cd9342d9ba9b54d13eb324327b5657d882311
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-10-05 17:21:29 +0530

    Create NER.py

---
+++ examples/streamlit/NER.py
@@ -0,0 +1,5 @@
+import nlu
+
+def layout():
+    nlu.enable_streamlit_caching() # Optional caching the models, recommended
+    nlu.load('ner').viz_streamlit_ner('Donald Trump from America and Angela Merkel from Germany do not share many views')
