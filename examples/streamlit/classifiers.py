9f9c08496aec4a3c5582257de4a7fe6c1d9b983f
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-10-05 17:21:19 +0530

    Create classifiers.py

---
+++ examples/streamlit/classifiers.py
@@ -0,0 +1,5 @@
+import nlu
+
+def layout():
+    nlu.enable_streamlit_caching() # Optional caching the models, recommended
+    nlu.load('sentiment').viz_streamlit_classes('I love NLU and Streamlit!')
