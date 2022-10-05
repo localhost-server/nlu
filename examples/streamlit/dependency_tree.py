2a176b788cf279d39149763dc16024976601a569
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-10-05 17:21:25 +0530

    Create dependency_tree.py

---
+++ examples/streamlit/dependency_tree.py
@@ -0,0 +1,5 @@
+import nlu
+
+def layout():
+    nlu.enable_streamlit_caching() # Optional caching the models, recommended
+    nlu.load('dep.typed').viz_streamlit_dep_tree('POS tags define a grammatical label for each token and the Dependency Tree classifies Relations between the tokens')
