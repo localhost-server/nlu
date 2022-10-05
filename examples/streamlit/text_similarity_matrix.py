a57a30afc132b6000c2d1e37210cb4abc269d0c9
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-10-05 17:21:34 +0530

    Create text_similarity_matrix.py

---
+++ examples/streamlit/text_similarity_matrix.py
@@ -0,0 +1,5 @@
+import nlu
+
+def layout():
+    nlu.enable_streamlit_caching() # Optional caching the models, recommended
+    nlu.load('ner').viz_streamlit_word_similarity(['I love love love  NLU !', 'I also love love love LOVE Streamlit'])
