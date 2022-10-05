74ef2d2a5cb53b7e3c33beaafc001349a3a57d1e
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-10-05 17:21:39 +0530

    Create token_features.py

---
+++ examples/streamlit/token_features.py
@@ -0,0 +1,4 @@
+import nlu
+def layout():
+    nlu.enable_streamlit_caching() # Optional caching the models, recommended
+    nlu.load('spell stem lemma pos').viz_streamlit_token('I liek pentut buttr and jelly !')
